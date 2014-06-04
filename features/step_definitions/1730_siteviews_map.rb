b167af25f9ab5cc8568fd81e47f1f7a07106db9f
gquotient <support@gquotient.com>
2014-06-04 16:44:29 +0530

    Update 1730_siteviews_map.rb

--- features/step_definitions/1730_siteviews_map.rb
+++ features/step_definitions/1730_siteviews_map.rb
@@ -1,27 +1,15 @@
-
-Given(/^I am in Site View page$/) do
- within (".table-container")do
-	page.find('tr', :text => "Lemoore Liberty Middle School").click
-  	sleep 20
-  end
-  click_link("Site Views")
-  sleep 20
+Given(/^I click the "(.*?)" page$/) do |name|
+ within (".table-container") do
+    while not @screen.exists "#{name}"
+    sleep 20
+    end
+ @screen.click "#{name}"
+ end
 end
 
-Given(/^I navigate to Map page$/) do
-  within("#project-site-header")do
-  	find("option[value='map']").click
+When(/^I click to "(.*?)" links$/) do |name|
+  while not @screen.exists "#{name}"
+    sleep 3
   end
-  sleep 20
-end
-
-Given(/^I verify Map is getting displayed$/) do
-  within("#project-site-content")do
-	  if page.has_content?("Clouds")
-	  	puts "Map is displayed"
-	  	page.save_screenshot('reports/siteviewsmap.png')
-	  else
-	  	puts "Map is not getting displayed"
-	  end
-  end
+ @screen.click "#{name}"
 end
